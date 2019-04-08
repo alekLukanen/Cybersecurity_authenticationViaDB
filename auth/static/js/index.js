839a5ee5e8910bd0d42b5c21176f93e94ebd3bfe
Carlos Espinoza <espinozc@augsburg.edu>
2019-04-07 21:01:49 -0500

    Adding javascript functions

--- auth/static/js/index.js
+++ auth/static/js/index.js
@@ -42,3 +42,28 @@ $('.tab a').on('click', function (e) {
   
 });
 
+
+function postRequest(username, password){
+  var xhr = new XMLHttpRequest();
+  
+  xhr.open("POST", token_url, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+  xhr.setRequestHeader('Accept', 'application/JSON');
+  xhr.send(JSON.stringify({ username: password}));
+
+  xhr.onreadystatechange = function(){
+    if(this.readyState != 4) return;
+
+    if (this.status == 200){
+       var data = JSON.parse(this.responseText);  
+       console.log(data);
+    }
+  }
+}
+
+
+
+// var Session = __class__ ('Session', [Object], {
+//     get __init__ () {return __get__ (this, function (self, token_url) {
+//       self.token_url = token_url;});
+//     }});
